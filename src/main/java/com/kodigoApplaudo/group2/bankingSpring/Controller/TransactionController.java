17f918e8f263200c212b5a98e919fb1936e28110
camjunior <camjunior90@gmail.com>
2022-08-09 19:28:56 -0300

    Added transaction controller and transacion services

---
+++ src/main/java/com/kodigoApplaudo/group2/bankingSpring/Controller/TransactionController.java
@@ -0,0 +1,25 @@
+package com.kodigoApplaudo.group2.bankingSpring.Controller;
+
+import java.math.BigDecimal;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.kodigoApplaudo.group2.bankingSpring.Model.Account;
+import com.kodigoApplaudo.group2.bankingSpring.Services.TransactionService;
+
+public class TransactionController {
+
+    private TransactionService transactionService;
+
+    @PostMapping("/deposit/{idConta}")
+    public Account deposit(BigDecimal amount, @PathVariable("id") Long id) throws Exception {
+        return transactionService.deposit(amount, id);
+    }
+
+    @PostMapping("/withdraw/{id}")
+    public Account withdraw(BigDecimal amount, @PathVariable("id") Long id) throws Exception {
+        return transactionService.withdraw(amount, id);
+    }
+
+}
