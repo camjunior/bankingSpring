1d05b31b095c89e1b1c733f64bc2bf393e662491
Felix <52052988+FeJoToRe@users.noreply.github.com>
2022-08-11 11:51:31 -0400

    Added controller for listing Accounts Added Query anotation for AccountRepository

--- src/main/java/com/kodigoApplaudo/group2/bankingSpring/Controller/AccountListController.java
+++ src/main/java/com/kodigoApplaudo/group2/bankingSpring/Controller/AccountListController.java
@@ -1,23 +1,25 @@
 package com.kodigoApplaudo.group2.bankingSpring.Controller;
 
 import com.kodigoApplaudo.group2.bankingSpring.Model.Account;
+import com.kodigoApplaudo.group2.bankingSpring.Model.Customer;
 import com.kodigoApplaudo.group2.bankingSpring.Repository.AccountRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api")
 public class AccountListController {
 
   @Autowired private AccountRepository accountRepository;
-  @GetMapping("/accounts")
-  public List<Account> getAllAccounts() {
-
-
-    return accountRepository.findAll();
+    @GetMapping("/customerAccounts")
+    public List<Account> getList(@RequestParam(name = "id") int id) {
+        List<Account> account = accountRepository.findByCustomerId(id);
+        return account;
+    }
   }
-}
+
--- src/main/java/com/kodigoApplaudo/group2/bankingSpring/Repository/AccountRepository.java
+++ src/main/java/com/kodigoApplaudo/group2/bankingSpring/Repository/AccountRepository.java
@@ -6,7 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface AccountRepository extends JpaRepository<Account, Long> {
-
+    @Query(value="SELECT * FROM Accounts a WHERE a.customer_id=?1", nativeQuery = true)
+    List<Account> findByCustomerId(int id);
 }
